a37fad08d2bd68a363ebd62efd8502bb09d835d7
Peter Ade-Ojo <peteradeojo@outlook.com>
2023-07-12 17:16:00 +0100

    jwt token for verify user endpoint

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,11 @@
-import express from "express";
-import passport from "passport";
+import express from 'express';
+import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import {
-  loginUser,
-  registerUser,
-  verifyUser,
-} from "../controllers/user.controller.js";
+	loginUser,
+	registerUser,
+	verifyUser,
+} from '../controllers/user.controller.js';
 
 // Created an express routing instance
 const userRouter = express.Router();
@@ -14,32 +14,34 @@ const userRouter = express.Router();
 // You can read more about it in Express Docs
 
 // To add New Users and Get all Existing Users Data
-userRouter.route("/").post(registerUser, (req, res, next) => {
-  passport.authenticate("jwt", function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      // *** Display message without using flash option
-      // re-render the login form with a message
-      return res.status(400).json({
-        error: info.message ?? "A User with the given username or email exists",
-      });
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      user.password = undefined;
+userRouter.route('/').post(registerUser, (req, res, next) => {
+	passport.authenticate('jwt', function (err, user, info) {
+		if (err) {
+			return next(err);
+		}
+		if (!user) {
+			// *** Display message without using flash option
+			// re-render the login form with a message
+			return res.status(400).json({
+				error: info.message ?? 'A User with the given username or email exists',
+			});
+		}
+		req.logIn(user, function (err) {
+			if (err) {
+				return next(err);
+			}
+			user.password = undefined;
 
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "1d",
-      });
-      return res.status(201).json({ user, token });
-    });
-  })(req, res, next);
+			const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+				expiresIn: '1d',
+			});
+			return res.status(201).json({ user, token });
+		});
+	})(req, res, next);
 }); // http://localhost:8080/api/user/
-userRouter.route("/login").post(loginUser);
-userRouter.route("/verify").post(verifyUser);
+userRouter.route('/login').post(loginUser);
+userRouter
+	.route('/verify')
+	.post(passport.authenticate('jwt', { session: false }), verifyUser);
 
 export default userRouter;
